a5084547268310375229573f6269d155a0e182c3
KateSkirnevskaya <skirnevskaya00@gmail.com>
2024-10-25 20:57:37 +0300

    fix: Дописана реализация методов в MyMap. Добавлен класс LinkedBinaryTree с тестами.Класс BinarySearchTree.

---
+++ src/test/java/BinarySearchTreeTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import search.BinarySearchTree;
+
+public class BinarySearchTreeTest {
+
+    @Test
+    void test() {
+        BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+
+    }
+}
